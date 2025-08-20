208b4e8f9fa986b92ff0577e94b8352f29698f68
Vaishnavi <vaishnavi@adhoonik.com>
2025-08-20 15:06:48 +0530

    failed test case fix

--- qtify/src/components/Carousel/Carousel.js
+++ qtify/src/components/Carousel/Carousel.js
@@ -1,4 +1,3 @@
-// src/components/Carousel/Carousel.js
 import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -11,52 +10,52 @@ import LeftNavIcon from "../../assets/left-nav.svg";
 import RightNavIcon from "../../assets/right-nav.svg";
 
 function Carousel({ data, type }) {
-  const swiperRef = useRef(null);
+  const swiperRef = useRef(null);
 
-  return (
-    <div className={styles.carouselContainer}>
-      {/* Custom navigation buttons, now positioned correctly */}
-      <div
-        className={`${styles.navButton} ${styles.prevButton}`}
-        onClick={() => swiperRef.current.slidePrev()}
-      >
-        <img src={LeftNavIcon} alt="Previous" className={styles.navIcon} />
-      </div>
-      <div
-        className={`${styles.navButton} ${styles.nextButton}`}
-        onClick={() => swiperRef.current.slideNext()}
-      >
-        <img src={RightNavIcon} alt="Next" className={styles.navIcon} />
-      </div>
+  return (
+    <div className={styles.carouselContainer}>
+      {/* Custom navigation buttons, now positioned correctly */}
+      <div
+        className={`${styles.navButton} ${styles.prevButton}`}
+        onClick={() => swiperRef.current.slidePrev()}
+      >
+        <img src={LeftNavIcon} alt="Previous" className={styles.navIcon} />
+      </div>
+      <div
+        className={`${styles.navButton} ${styles.nextButton}`}
+        onClick={() => swiperRef.current.slideNext()}
+      >
+        <img src={RightNavIcon} alt="Next" className={styles.navIcon} />
+      </div>
 
-      {/* Swiper component wrapped in a div to manage its own padding */}
-      <div className={styles.swiperWrapper}>
-        <Swiper
-          modules={[Navigation]}
-          spaceBetween={20}
-          slidesPerView={6}
-          slidesPerGroup={1} 
-          onSwiper={(swiper) => {
-            swiperRef.current = swiper;
-          }}
-          breakpoints={{
-            320: { slidesPerView: 2 },
-            480: { slidesPerView: 3 },
-            640: { slidesPerView: 4 },
-            768: { slidesPerView: 5 },
-            1024: { slidesPerView: 6 },
-            1440: { slidesPerView: 6 },
-          }}
-        >
-          {data.map((item) => (
-            <SwiperSlide key={item.id}>
-              <Card data={item} isSong={type === "song"} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </div>
-  );
+      {/* Swiper component wrapped in a div to manage its own padding */}
+      <div className={styles.swiperWrapper}>
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={20}
+          slidesPerView={7}
+          slidesPerGroup={1} // Add this line
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          breakpoints={{
+            320: { slidesPerView: 2, slidesPerGroup: 1 },
+            480: { slidesPerView: 3, slidesPerGroup: 1 },
+            640: { slidesPerView: 4, slidesPerGroup: 1 },
+            768: { slidesPerView: 5, slidesPerGroup: 1 },
+            1024: { slidesPerView: 6, slidesPerGroup: 1 },
+            1440: { slidesPerView: 7, slidesPerGroup: 1 },
+          }}
+        >
+          {data.map((item) => (
+            <SwiperSlide key={item.id}>
+              <Card data={item} isSong={type === "song"} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 }
 
 export default Carousel;
